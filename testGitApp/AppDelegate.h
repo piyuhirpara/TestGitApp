b192040df722befa37484966b8f80126292ae724
Piyu Hirpara <piyu.hirpara@gmail.com>
2013-10-19 15:00:12 +0530

    Initial Commit

---
+++ testGitApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testGitApp
+//
+//  Created by Piyu Hirpara on 19/10/13.
+//  Copyright (c) 2013 Piyu Hirpara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testGitApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testGitApp
+//
+//  Created by Piyu Hirpara on 19/10/13.
+//  Copyright (c) 2013 Piyu Hirpara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
